74cca29cf6b4b05c6a5ef85b3ad54c9098027a8d
ShoheiTakino <104742356+ShoheiTakino@users.noreply.github.com>
2022-07-05 09:48:36 +0900

    propsでの入力値受け渡しとスタイル当て

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import "./styles.css";
 
 export default function App() {
   console.log("aaa");
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
   const onClickCountButton = () => {
     setCount(count + 1);
   };
 
+  const onClickOpen = () => setOpen(!open);
+
   return (
     <div className="App">
-      <input />
-      <button>表示</button>
-      <ChildArea />
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
     </div>
   );
 }
